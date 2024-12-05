01534cf9048da56f16b935a3a45e292b75047346
neko-jarasi <157803564+neko-jarasi@users.noreply.github.com>
2024-12-05 21:58:24 +0900

    Update memomain.js

--- memomain.js
+++ memomain.js
@@ -1,22 +1,7 @@
-document.getElementById('set').onclick = function() {
-  let usermemo = document.getElementById('memo').value
-  let usertitle = document.getElementById('title').value
-  localStorage.setItem('localmemo', usermemo);
-  localStorage.setItem('localtitle',usertitle)
-  alert('保存しました')
-};
-
-document.getElementById('get').onclick = function() {
-  document.getElementById('memo').value=localStorage.getItem('localmemo')
-  document.getElementById('title').value=localStorage.getItem('localtitle')
-  
-  alert('タイトル:'+localStorage.getItem('localtitle')+'\n内容:'+localStorage.getItem('localmemo'))
-};
-
-document.getElementById('clear').onclick=function(){
-  document.getElementById('memo').value=''
-  document.getElementById('title').value=''
-  localStorage.removeItem('localmemo')
-  localStorage.removeItem('localtitle')
-  alert('削除出来ました')
-}
+document.getElementById('memo').addEventListener("input",function(){
+    let usermemo = document.getElementById('memo').value
+    localStorage.setItem('localmemo', usermemo);
+  })
+  window.onload = function() {
+    document.getElementById('memo').value=localStorage.getItem('localmemo')
+  };
